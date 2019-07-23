4abde9ad3391d9aa4d9c33ca01f160de1d33d76e
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-07-23 03:33:40 -0700

    changed bbackgorund to white and letterings to black. Will add finishing touches and contact page tomrrow!;

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import '../styles/Contact.css';
 
 class Contact extends React.Component {
   render() {
     return(
       <div>
         <Navbar />
-        
+          <h1>Bowse through our products and click below to place your order. We Will respond promptly via email and have your order ready in no time!</h1>
+        <a class="email-link" href="mailto:yohanes2o6@gmail.com">place order here</a>
         <Footer />
       </div>
     )
