87b8f8f6349190b84110da7fd7991312dc1f62c6
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-07-23 01:04:44 -0700

    added transparent images to home page. turning them to links now

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+
+import beans from '../media/bbqpintobeans.png';
+import seasonings from '../media/bundle-one.png';
+import apparel from '../media/bbq-shirt.png';
 import '../styles/Home.css'
 
 
@@ -10,9 +14,31 @@ function Home() {
       <Navbar />
         <div className="title">Where Southern Comfort Begins</div>
         <hr className="home-hr" />
-      <div className="home-container">
-        <div className="home-text">Our products consist of the necessary foundations for your favorite southern dishes. From our famous hickory seasonings to our smokey savoury snacks, we put of every bit of southern flavor in every bite. </div>
-      </div>
+        <div className="home-container">
+          <div className="home-text">Our products consist of the necessary foundations for your favorite southern dishes. From our famous hickory seasonings to our smokey savoury snacks, we put of every bit of southern flavor in every bite. </div>
+        </div>
+
+        <hr className="home-hr" />
+
+        <div className="products-shortcut">
+          <div className="seasonings">
+              Seasonings
+            <div className="seasonings-img"></div>
+          </div>
+
+          <div className="beans">
+            Beans
+            <div className="beans-img"></div>
+          </div>
+
+          <div className="apparel">
+            Apparel
+            <div className="apparel-img" alt="apparel"></div>
+          </div>
+        </div>
+
+      <hr className="home-hr" />
+
       <Footer />
     </div>
   )
--- src/components/Products.js
+++ src/components/Products.js
@@ -13,102 +13,114 @@ import BundleTwo from '../media/bundle-two.png';
 
 import '../styles/Products.css'
 
-function Products() {
-  return(
-    <div>
-      <Navbar />
-      <div className="products-title">Seasonings</div>
-      <div className="products-container">
-        <div>
-          <img className="product-image" src={BoldBeef} alt="Mr. Texas Glorious Garlic Pinto Beans" />
-          <div className="product-description">
-            {Data.spices.boldBeef.title}
-          </div>
-          <hr className="product-break" />
-          <div className="product-description">
-            ${Data.spices.boldBeef.price}
-          </div>
-        </div>
+class Products extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cart: 0
+    }
+  }
 
-        <div >
-          <img className="product-image" src={RoyalPork} alt="Mr. Texas Radiant Red Beans" />
-          <div className="product-description">
-            {Data.spices.royalPork.title}
+  // handleClick() {
+  //   this.setState({ price: })
+  // }
+  render() {
+    return(
+      <div>
+        <Navbar />
+        <div className="products-title">Seasonings</div>
+        <div className="products-container">
+          <div>
+            <img className="product-image" src={BoldBeef} alt="Mr. Texas Glorious Garlic Pinto Beans" />
+            <div className="product-description">
+              {Data.spices.boldBeef.title}
             </div>
-          <hr className="product-break" />
-          <div className="product-description">
-            ${Data.spices.royalPork.price}
+            <hr className="product-break" />
+            <div className="product-description">
+              ${Data.spices.boldBeef.price}
             </div>
+          </div>
+  
+          <div >
+            <img className="product-image" src={RoyalPork} alt="Mr. Texas Radiant Red Beans" />
+            <div className="product-description">
+              {Data.spices.royalPork.title}
+              </div>
+            <hr className="product-break" />
+            <div className="product-description">
+              ${Data.spices.royalPork.price}
+              </div>
+          </div>
+  
+          <div >
+            <img className="product-image" src={SavoryPoultry} alt="Mr. Texas Radiant Red Beans" />
+            <div className="product-description">
+              {Data.spices.savoryPoultry.title}
+              </div>
+            <hr className="product-break" />
+            <div className="product-description">
+              ${Data.spices.savoryPoultry.price}
+              </div>
+          </div>
         </div>
-
-        <div >
-          <img className="product-image" src={SavoryPoultry} alt="Mr. Texas Radiant Red Beans" />
-          <div className="product-description">
-            {Data.spices.savoryPoultry.title}
+  
+        <div className="products-title">Beans</div>
+        <div className="products-container">
+          <div>
+          <img className="product-image" src={PintoBeans} alt="Mr. Texas Glorious Garlic Pinto Beans" />
+            <div className="product-description">
+              {Data.beans.garlicPinto.title}
             </div>
-          <hr className="product-break" />
-          <div className="product-description">
-            ${Data.spices.savoryPoultry.price}
+            <hr className="product-break"/>
+            <div className="product-description">
+              ${Data.beans.garlicPinto.price}
             </div>
+            <button className="purchase-button">add to cart</button>
+          </div>
+  
+          <div>
+            <img className="product-image" src={RedBeans} alt="Mr. Texas Radiant Red Beans" />
+            <div className="product-description">
+              {Data.beans.radiantRed.title}
+              </div>
+            <hr className="product-break" />
+            <div className="product-description">
+              ${Data.beans.radiantRed.price}
+              </div>
+          </div>
         </div>
+  
+        <div className="products-title">Bundles</div>
+        <div className="products-container">
+          <div>
+            <img className="product-image" src={BundleOne} alt="Mr. Texas Glorious Garlic Pinto Beans" />
+            <div className="product-description">
+              {Data.beans.garlicPinto.title}
+            </div>
+            <hr className="product-break" />
+            <div className="product-description">
+              ${Data.beans.garlicPinto.price}
+  
+            </div>
+          </div>
+  
+          <div className="bundle-two">
+            <img className="product-image" src={BundleTwo} alt="Mr. Texas Radiant Red Beans" />
+  
+            <div className="product-description">
+              Radiant Red Beans
+              </div>
+            <hr className="product-break" />
+            <div className="product-description">
+              $6.99
+              </div>
+          </div>
+  
+        </div>
+        <Footer />
       </div>
-
-      <div className="products-title">Beans</div>
-      <div className="products-container">
-        <div>
-        <img className="product-image" src={PintoBeans} alt="Mr. Texas Glorious Garlic Pinto Beans" />
-          <div className="product-description">
-            {Data.beans.garlicPinto.title}
-          </div>
-          <hr className="product-break"/>
-          <div className="product-description">
-            ${Data.beans.garlicPinto.price}
-          </div>
-          <button className="purchase-button">add to cart</button>
-        </div>
-
-        <div>
-          <img className="product-image" src={RedBeans} alt="Mr. Texas Radiant Red Beans" />
-          <div className="product-description">
-            {Data.beans.radiantRed.title}
-            </div>
-          <hr className="product-break" />
-          <div className="product-description">
-            ${Data.beans.radiantRed.price}
-            </div>
-        </div>
-      </div>
-
-      <div className="products-title">Bundles</div>
-      <div className="products-container">
-        <div>
-          <img className="product-image" src={BundleOne} alt="Mr. Texas Glorious Garlic Pinto Beans" />
-          <div className="product-description">
-            {Data.beans.garlicPinto.title}
-          </div>
-          <hr className="product-break" />
-          <div className="product-description">
-            ${Data.beans.garlicPinto.price}
-
-          </div>
-        </div>
-
-        <div className="bundle-two">
-          <img className="product-image" src={BundleTwo} alt="Mr. Texas Radiant Red Beans" />
-
-          <div className="product-description">
-            Radiant Red Beans
-            </div>
-          <hr className="product-break" />
-          <div className="product-description">
-            $6.99
-            </div>
-        </div>
-
-      </div>
-      <Footer />
-    </div>
-  )
+    )
+  }
 }
 
 export default Products;
